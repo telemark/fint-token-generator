93b0676608a41ef9ed61433e93c73a1c65164bee
Jonas Enge <jonas.enge@gmail.com>
2018-01-09 11:36:08 +0100

    Genesis commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = async opts => {
     throw Error('Missing required input: options')
   }
   if (!opts.url) {
-    throw Error('Missing required input: options.url')
+    throw Error('Missing required input: url')
   }
   if (!opts.credentials) {
     throw Error('Missing required input: options.credentials')
@@ -14,8 +14,20 @@ module.exports = async opts => {
   if (!opts.credentials.auth) {
     throw Error('Missing required input: options.credentials.auth')
   }
+  if (!opts.credentials.auth.username) {
+    throw Error('Missing required input: username')
+  }
+  if (!opts.credentials.auth.password) {
+    throw Error('Missing required input: password')
+  }
   if (!opts.credentials.client) {
     throw Error('Missing required input: options.credentials.client')
+  }
+  if (!opts.credentials.client.client_id) {
+    throw Error('Missing required input: client_id')
+  }
+  if (!opts.credentials.client.client_secret) {
+    throw Error('Missing required input: client_secret')
   }
 
   const { url, credentials } = opts
